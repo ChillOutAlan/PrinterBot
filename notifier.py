d882516e6663edd7e65c25dcc50aec2be62b762c
ChilloutAlan <nala.irt.neyugn@gmail.com>
2019-01-16 23:49:48 -0500

    printer bot v.1

---
+++ notifier.py
@@ -0,0 +1,146 @@
+from slackclient import SlackClient
+import scrapemodnot
+import mysql.connector as mariadb
+import unicodedata
+from datetime import datetime
+color = "xxx"
+bw = 'xxx'
+
+mariadb_connection = mariadb.connect(user='x', password='xxx', database='xxx') # config file
+cursor = mariadb_connection.cursor()
+
+def slack_message(message, channel):
+    access_token = "xxx" #Also Export Token
+    sc = SlackClient(access_token)
+    sc.api_call('chat.postMessage', channel=channel, text=message, username='Notifier Bot')
+
+# Notify on Replacing the Ink Cartridge for the Color Printer
+def catridge_notification():
+    cartridge_list = [scrapemodnot.printer_stats(color)['yellow'],scrapemodnot.printer_stats(color)['cyan'],scrapemodnot.printer_stats(color)['black'], scrapemodnot.printer_stats(color)['magenta']]
+    cartridge_names = ["`yellow status`", "`cyan status`", "`black status`", "`magenta status`"]
+    cartridge_stats = [scrapemodnot.printer_stats(color)['yellowst'],scrapemodnot.printer_stats(color)['cyanst'],scrapemodnot.printer_stats(color)['blackst'], scrapemodnot.printer_stats(color)['magentast']]
+    #COLOR PRINTER Catridge Notifier
+    # If Cartridge != DB Row don't push
+    for a in range(len(cartridge_list)):
+        query_statement = "select {} from COLORPRINTER LIMIT 1;".format(cartridge_names[a])
+        cursor.execute(query_statement)
+        data = cursor.fetchone()
+        data = ''.join(data)
+        if data != cartridge_list[a]:
+            slack_message("{0}".format(cartridge_stats[a]), "bot-tester")
+            update_state = "UPDATE COLORPRINTER SET {0} = '{1}';".format(cartridge_names[a], cartridge_list[a])
+            cursor.execute(update_state)
+            # Time update
+            dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            time_statement = "UPDATE COLORPRINTER SET `DATE TIME` = '{0}'".format(dt)
+            cursor.execute(time_statement)
+            mariadb_connection.commit()
+            print("Pushed Message and Updated Database")
+        else:
+            #Push Notification if x > 1 hour and no changes
+            query1_statement = "select `DATE TIME` from COLORPRINTER LIMIT 1;"
+            cursor.execute(query1_statement)
+            newtime = cursor.fetchone()
+            olddata = "".join(str(v) for v in newtime)
+            date_old = datetime.strptime(olddata,"%Y-%m-%d %H:%M:%S")
+            date_new = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            date_new = datetime.strptime(date_new, "%Y-%m-%d %H:%M:%S")
+            time = date_new - date_old
+            if time.total_seconds() > 18000 and cartridge_list[a] != "OK": # 18000 seconds = 5 hours
+                slack_message("{0}".format(cartridge_stats[a]), "bot-tester")
+                dt1 = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                time_statement1 = "UPDATE COLORPRINTER SET `DATE TIME` = '{0}'".format(dt1)
+                cursor.execute(time_statement1)
+                mariadb_connection.commit()
+                print("Updated Time in Database!")
+    return None
+
+# Give status on Drum if there are issues
+#drum_stats = [scrapemodnot.printer_stats(color)['yellowdrumst'],scrapemodnot.printer_stats(color)['cyandrumst'],scrapemodnot.printer_stats(color)['blackdrumst'], scrapemodnot.printer_stats(color)['magdrumst']]
+def drum_notification():
+    cartridge_list = [scrapemodnot.printer_stats(color)['yellowdrum'],scrapemodnot.printer_stats(color)['cyandrum'],scrapemodnot.printer_stats(color)['blackdrum'], scrapemodnot.printer_stats(color)['magdrum'], scrapemodnot.printer_stats(color)['wastebox']]
+    cartridge_names = ["`yellow drum status`", "`cyan drum status`", "`black drum status`", "`magenta drum status`", "`wastebox status`"]
+    cartridge_stats = [scrapemodnot.printer_stats(color)['yellowdrumst'],scrapemodnot.printer_stats(color)['cyandrumst'],scrapemodnot.printer_stats(color)['blackdrumst'], scrapemodnot.printer_stats(color)['magdrumst'], scrapemodnot.printer_stats(color)['wastebox']]
+    #COLOR PRINTER Catridge Notifier
+    # If Cartridge != DB Row don't push
+    for a in range(len(cartridge_list)):
+        query_statement = "select {} from COLORPRINTER LIMIT 1;".format(cartridge_names[a])
+        cursor.execute(query_statement)
+        data = cursor.fetchone()
+        data = ''.join(data)
+        if data != cartridge_list[a]:
+            slack_message("{0}".format(cartridge_stats[a]), "bot-tester")
+            update_state = "UPDATE COLORPRINTER SET {0} = '{1}';".format(cartridge_names[a], cartridge_list[a])
+            cursor.execute(update_state)
+            # Time update
+            dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            time_statement = "UPDATE COLORPRINTER SET `DATE TIME` = '{0}'".format(dt)
+            cursor.execute(time_statement)
+            mariadb_connection.commit()
+            print("Pushed Message and Updated Database")
+        else:
+            #Push Notification if x > 1 hour and no changes
+            query1_statement = "select `DATE TIME` from COLORPRINTER LIMIT 1;"
+            cursor.execute(query1_statement)
+            newtime = cursor.fetchone()
+            olddata = "".join(str(v) for v in newtime)
+            date_old = datetime.strptime(olddata,"%Y-%m-%d %H:%M:%S")
+            date_new = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            date_new = datetime.strptime(date_new, "%Y-%m-%d %H:%M:%S")
+            time = date_new - date_old
+            if time.total_seconds() > 18000 and cartridge_list[a] != "OK": # 18000 seconds = 5 hours
+                slack_message("{0}".format(cartridge_stats[a]), "bot-tester")
+                dt1 = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                time_statement1 = "UPDATE COLORPRINTER SET `DATE TIME` = '{0}'".format(dt1)
+                cursor.execute(time_statement1)
+                mariadb_connection.commit()
+                print("Updated Time in Database!")
+    return None
+    
+# Tray Status for Color PRINTER
+#'tray1':tray_response,'tray2':tray2_response, 'tray_status':tray1, 'tray2_status':tray2}
+def paper_notfication():
+    cartridge_list = [scrapemodnot.printer_stats(color)['tray_response'],scrapemodnot.printer_stats(color)['tray2_response']]
+    cartridge_names = ["`TRAY STATUS 1`", "`TRAY STATUS 2`"]
+    cartridge_stats = [scrapemodnot.printer_stats(color)['tray_status'],scrapemodnot.printer_stats(color)['tray2_status']]
+    #COLOR PRINTER Catridge Notifier
+    # If Cartridge != DB Row don't push
+    for a in range(len(cartridge_list)):
+        query_statement = "select {} from COLORPRINTER LIMIT 1;".format(cartridge_names[a])
+        cursor.execute(query_statement)
+        data = cursor.fetchone()
+        data = ''.join(data)
+        if data != cartridge_list[a]:
+            slack_message("{0}".format(cartridge_stats[a]), "bot-tester")
+            update_state = "UPDATE COLORPRINTER SET {0} = '{1}';".format(cartridge_names[a], cartridge_list[a])
+            cursor.execute(update_state)
+            # Time update
+            dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            time_statement = "UPDATE COLORPRINTER SET `DATE TIME` = '{0}'".format(dt)
+            cursor.execute(time_statement)
+            mariadb_connection.commit()
+            print("Pushed Message and Updated Database")
+        else:
+            #Push Notification if x > 1 hour and no changes
+            query1_statement = "select `DATE TIME` from COLORPRINTER LIMIT 1;"
+            cursor.execute(query1_statement)
+            newtime = cursor.fetchone()
+            olddata = "".join(str(v) for v in newtime)
+            date_old = datetime.strptime(olddata,"%Y-%m-%d %H:%M:%S")
+            date_new = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            date_new = datetime.strptime(date_new, "%Y-%m-%d %H:%M:%S")
+            time = date_new - date_old
+            if time.total_seconds() > 18000 and cartridge_list[a] != "OK": # 18000 seconds = 5 hours
+                slack_message("{0}".format(cartridge_stats[a]), "bot-tester")
+                dt1 = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                time_statement1 = "UPDATE COLORPRINTER SET `DATE TIME` = '{0}'".format(dt1)
+                cursor.execute(time_statement1)
+                mariadb_connection.commit()
+                print("Updated Time in Database!")
+    return None
+
+if __name__ == '__main__':
+    catridge_notification()
+    drum_notification()
+    paper_notfication()
+    mariadb_connection.close()
---
+++ scrapemodnot.py
@@ -0,0 +1,208 @@
+import requests
+import lxml.html as lh
+
+#Color Printer
+def printer_stats(address):
+
+    #Create a handle, page, to handle the contents of the website
+    #Status page URL
+
+    page = requests.get(address)
+
+    #Store the contents of the website under doc
+    doc = lh.fromstring(page.content)
+
+    #Parse data that are stored between <tr>..</tr> of HTML
+    tr_elements = doc.xpath('//tr')
+
+    #print([len(T) for T in tr_elements[:12]])
+
+    #Create empty list
+    col=[]
+    i=0
+    #For each row, store each first element (header) and an empty list
+    for t in tr_elements[0]:
+        i+=1
+        name=t.text_content()
+        #print('%d:"%s"'%(i,name))
+        col.append((name))
+    #print(col[0])
+    str1 = ''.join(col).encode('utf-8').strip() # adding encoding or will have unicode error .encode('utf-8').strip()
+    str1 = str1.replace('\r', '') # removes \r from string
+    str2 = str1.replace('\n', '') # removes \n from string
+
+    #find the string positions of each of the cartridge levels
+    cyanpos = str2.find('Cyan Toner Cartridge Level')
+    magentapos = str2.find('Magenta Toner Cartridge Level')
+    yellowpos = str2.find('Yellow Toner Cartridge Level')
+    blackpos = str2.find('Black Toner Cartridge Level')
+
+    #cartridge level
+    cyanst = str2[cyanpos:magentapos]
+    magentast = str2[magentapos:yellowpos]
+    yellowst = str2[yellowpos:blackpos]
+    blackst = str2[blackpos:355] #335 Position
+    #cartridge_list = '{0}\n{1}\n{2}\n{3}'.format(cyan, magenta, yellow, black)
+
+    #print(cyan,'\n', magenta, '\n', yellow, '\n', black)
+
+    if "OK" in cyanst:
+        cyan = "OK"
+    else:
+        cyan = "Low"
+    if "OK" in magentast:
+        magenta = "OK"
+    else:
+        magenta = "Low"
+    if "OK" in yellowst:
+        yellow = "OK"
+    else:
+        yellow = "Low"
+    if "OK" in blackst:
+        black = "OK"
+    else:
+        black = "Low"
+
+
+    #cartridge drum positions
+    cyandrumpos = str2.find('Cyan Drum Cartridge')
+    magdrumpos = str2.find('Magenta Drum Cartridge')
+    yellowdrumpos = str2.find('Yellow Drum Cartridge')
+    blackdrumpos = str2.find('Black Drum Cartridge')
+    wastedrumpos = str2. find('Waste Toner Box')
+
+    #The Cartridge Drum Levels
+    cyandrumst = str2[cyandrumpos:magdrumpos]
+    magdrumst = str2[magdrumpos:yellowdrumpos]
+    yellowdrumst = str2[yellowdrumpos:blackdrumpos]
+    blackdrumst = str2[blackdrumpos:wastedrumpos]
+    wasteboxst = str2[wastedrumpos:495]
+    #drum_list = '{0}\n{1}\n{2}\n{3}\n{4}'.format(cyandrum, magdrum, yellowdrum, blackdrum, wastebox)
+
+    if "OK" in cyandrumst:
+        cyandrum = "OK"
+    else:
+        cyandrum = "Low"
+    if "OK" in magdrumst:
+        magentadrum = "OK"
+    else:
+        magentadrum = "Low"
+    if "OK" in yellowdrumst:
+        yellowdrum = "OK"
+    else:
+        yellowdrum = "Low"
+    if "OK" in blackdrumst:
+        blackdrum = "OK"
+    else:
+        blackdrum = "Low"
+    if "OK" in wasteboxst:
+        wastebox = "OK"
+    else:
+        wastebox = "Low"
+
+    #find the string positions of the Paper Capacity
+    tray1pos = str2.find('Tray 1')
+    tray2pos = str2.find('Tray 2')
+
+    #Paper Level (If string contains ok return paper level is good and if string contains low paper return fill paper)
+    tray1 = str2[tray1pos:tray2pos] #
+    tray2 = str2[tray2pos:616]
+
+    if "Low" in tray1:
+        tray_response = "Low"
+    elif "Add" in tray1:
+        tray_response = "Add"
+    elif "OK" in tray1:
+       tray_response = "OK"
+    if "Add" in tray2:
+        tray2_response = "Add"
+    elif "Low" in tray2:
+        tray2_response = "Low"
+    elif "OK" in tray2:
+        tray2_response = "OK"
+
+    #'tr':tray_response, 'tr2':tray2_response
+    #tray_status = '{0}\n {1}'.format(tray_response, tray2_response)
+    #return {'cl':cartridge_list, 'dl':drum_list, , 'ts': tray_status}
+    return {'yellow': yellow, 'cyan': cyan, 'magenta': magenta, 'black': black,
+    'yellowdrum': yellowdrum, 'cyandrum': cyandrum, 'magdrum': magentadrum, 'blackdrum': blackdrum,
+    'waste':wastebox, 'yellowst': yellowst, 'cyanst': cyan, 'magentast': magentast, 'blackst': blackst,
+    'yellowdrumst': yellowdrumst, 'cyandrumst': cyandrumst, 'magdrumst': magdrumst, 'blackdrumst': blackdrumst,
+    'wastest':wasteboxst, 'tray1':tray_response,'tray2':tray2_response, 'tray_status':tray1, 'tray2_status':tray2}
+
+"""
+page = requests.get('http://10.230.38.22/status/events.htm')
+
+#Store the contents of the website under doc
+doc = lh.fromstring(page.content)
+
+#Parse data that are stored between <tr>..</tr> of HTML
+tr_elements = doc.xpath('//tr')
+
+#print([len(T) for T in tr_elements[:12]])
+
+#Create empty list
+col=[]
+i=0
+#For each row, store each first element (header) and an empty list
+for t in tr_elements[0]:
+    i+=1
+    name=t.text_content()
+    #print('%d:"%s"'%(i,name))
+    col.append((name))
+print(col)
+"""
+#Black and White Printer
+def blackandwhite(bw):
+    #Create a handle, page, to handle the contents of the website
+    page = requests.get(bw)
+
+    #Store the contents of the website under doc
+    doc = lh.fromstring(page.content)
+
+    #Parse data that are stored between <tr>..</tr> of HTML
+    tr_elements = doc.xpath('//tr')
+
+    #print([len(T) for T in tr_elements[:12]])
+
+    #Create empty list
+    col=[]
+    i=0
+    #For each row, store each first element (header) and an empty list
+    for t in tr_elements:
+        i+=1
+        name=t.text_content()
+        #print('%d:"%s"'%(i,name))
+        col.append((name))
+
+    str1 = ''.join(col)
+    str1 = str1.replace('\r', '') # removes \r from string
+    str2 = str1.replace('\n', '') # removes \n from string
+
+    #Cartrdige Percentage
+    blackpos = str2.find('Black Cartridge')
+    perpos = str2.find('%')
+    black = str2[blackpos: perpos+1]
+    #print(black)
+
+    #Paper Levels
+    tray1pos = str2.find('Tray 1')
+    tray1 = str2[tray1pos:680]
+    tray2pos = str2.find('Tray 2')
+    tray2 = str2[tray2pos:720]
+
+    #print(tray2)
+    if 'OK' in tray1:
+        icetray1 = 'Paper Levels are OK!'
+    elif 'Missing' in tray1:
+        icetray1 = 'Paper is out. Please add paper.'
+    elif 'Low Paper' in tray1:
+        icetray1 = 'Paper is low. Add some paper please.'
+    if 'OK' in tray2:
+        icetray2 = 'Paper Levels are OK!'
+    elif 'Missing' in tray2:
+        icetray2 = 'Paper is out. Please add paper.'
+    elif 'Low Paper' in tray2:
+        icetray2 = 'Paper is low. Add some paper please.'
+    tray_status = '{0}\n {1}'.format(icetray1, icetray2)
+    return {'bl':black, 'ts':tray_status}
